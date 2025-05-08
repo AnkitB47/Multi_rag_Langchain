1eca4dbbf0d63952b2e01c913aaa3616bd8d1bba
AnkitB47 <jha.ankit230@example.com>
2025-05-08 17:52:35 +0200

    🚀 Pyhton env path selected

--- src/langgraphagenticai/vectorstore/pinecone_store.py
+++ src/langgraphagenticai/vectorstore/pinecone_store.py
@@ -1,21 +1,36 @@
-import os
+# src/langgraphagenticai/vectorstore/pinecone_store.py
+
 from pinecone import Pinecone, ServerlessSpec
-from langchain_pinecone import PineconeVectorStore
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+import os
 
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+PINECONE_REGION = os.getenv("PINECONE_REGION")
 
-def get_vectordb(pdf_path):
-    pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-    index_name = os.getenv("PINECONE_INDEX_NAME")
-    region = os.getenv("PINECONE_REGION")
-
-    if index_name not in pc.list_indexes().names():
+def get_vectordb():
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    
+    if PINECONE_INDEX_NAME not in pc.list_indexes().names():
         pc.create_index(
-            name=index_name,
+            name=PINECONE_INDEX_NAME,
             dimension=384,
             metric="cosine",
-            spec=ServerlessSpec(cloud="aws", region=region),
+            spec=ServerlessSpec(cloud="aws", region=PINECONE_REGION)
         )
-    index = pc.Index(index_name)
-    return PineconeVectorStore(index=index, embedding=embedding_model, namespace="default", text_key="text")
+
+    index = pc.Index(PINECONE_INDEX_NAME)
+
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vectordb = PineconeVectorStore(
+        index=index,
+        embedding=embedding_model,
+        namespace="default",
+        text_key="text"
+    )
+
+    return vectordb
